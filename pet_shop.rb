819e77d1e794e2723e4e5eaa667e223e7bc67c76
Andrew Binnie <binnieandrew@gmail.com>
2019-11-08 17:11:35 +0000

    adding the finding of pets y name and breed

--- pet_shop.rb
+++ pet_shop.rb
@@ -34,9 +34,19 @@ def pets_by_breed(pet_shop, breed)
       return breed_of_pet
 end
 
+def find_pet_by_name(pet_shop, name)
+    for pet in pet_shop[:pets]
+      if pet[:name] == name
+        return pet
+      end
+    end
+    return nil
+end
 
-# def pets_by_breed(pet_shop, breed)
-#   breed_of_pet = []
-#     for
-#     end
-# end
+def remove_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+      pet.shift()
+    end
+  end
+end
