48ba497ee168356d0d2d2b3a949acd0e7deddcd0
Andrew Binnie <binnieandrew@gmail.com>
2019-11-08 14:09:35 +0000

    adding in cash, sold pets & stock count

--- pet_shop.rb
+++ pet_shop.rb
@@ -5,3 +5,20 @@ end
 def total_cash(cash)
   return @pet_shop[:admin][:total_cash]
 end
+
+def add_or_remove_cash(pet_shop, cash)
+  @pet_shop[:admin][:total_cash] += cash
+  return @pet_shop[:admin][:total_cash]
+end
+
+def pets_sold(pet_shop)
+  return @pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, sold_pets)
+  @pet_shop[:admin][:pets_sold] += sold_pets
+end
+
+def stock_count(pet_shop)
+  return @pet_shop[:pets].length
+end
